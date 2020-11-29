3e87d4fcdbc36658da8c074036a42f4148af6ca9
PawelKonie99 <pawel.konieczny21@gmail.com>
2020-11-29 16:35:47 +0100

    implemented pagination in routes

--- controllers/memes.js
+++ controllers/memes.js
@@ -1,9 +1,14 @@
 const notesRouter = require("express").Router();
 const Meme = require("../models/meme");
+const middleware = require("../utils/middleware");
 
-notesRouter.get("/", async (req, res) => {
-  const memes = await Meme.find({});
-  res.json(memes);
+// notesRouter.get("/", async (req, res) => {
+//   const memes = await Meme.find({});
+//   res.json(memes);
+// });
+
+notesRouter.get("/", middleware.paginatedResults(Meme), (req, res) => {
+  res.json(res.paginatedResults);
 });
 
 notesRouter.get("/:id", async (req, res) => {
