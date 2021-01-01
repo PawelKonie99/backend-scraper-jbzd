950a67abd38e850cfa4dfc5c87b5faa3ad26c0a4
PawelKonie99 <pawel.konieczny21@gmail.com>
2021-01-01 20:54:47 +0100

    edit cron

--- app.ts
+++ app.ts
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === "scrap") {
   runScrap();
 }
 
-cron.schedule('0 */4 * * *', () => {
+cron.schedule('0 */1 * * *', () => {
   console.log('cron works every hour')
   runScrap();
 });
